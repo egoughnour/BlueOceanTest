002b6d52185448273a025a955875606c6164c466
Goughnour, Erik <e.goughnour@gmail.com>
2017-10-05 11:51:52 -0500

    adding another script for stubbing

---
+++ scripts/TurnToStone.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Message regarding deploy step... as well as"
+Write-Host -ForegroundColor Green "Everything that is alive" 
+Write-Host -ForegroundColor Blue -BackgroundColor White "In my Blue World"
